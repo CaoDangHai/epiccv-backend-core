a2e854c9b0bc9005c4908b80b56c5ee52215a493
Huynh Xuan Quoc Viet <Vietp456huynh@gmail.com>
2026-04-14 23:16:13 +0700

    Init NestJS backend: yarn, config, swagger

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('EpicCV Backend API')
+    .setDescription('API documentation for EpicCV backend')
+    .setVersion('1.0')
+    .addTag('epiccv')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT || 3000);
+}
+bootstrap();
